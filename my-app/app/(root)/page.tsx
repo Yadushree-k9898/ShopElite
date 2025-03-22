7fdc8ba0f0b697c8d029692d7a7af525df4dedc5
yadu <kyadushree47@gmail.com>
2025-03-22 11:13:54 +0530

    sample data and product list added

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,7 +1,10 @@
-
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 export default function Home() {
   return (
-   <>ShopElite</>
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals"  limit={4}/>
+    </>
   );
 }
