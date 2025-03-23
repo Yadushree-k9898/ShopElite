b96db77f8273f60d6057d9a90bc0a138e8e4a3b1
yadu <kyadushree47@gmail.com>
2025-03-23 13:55:26 +0530

    sign-in done

--- my-app/app/(auth)/sign-in/credentials-signin-form.tsx
+++ my-app/app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,16 +1,40 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { signInDefaultValues } from "@/lib/constants";
+import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useSearchParams } from "next/navigation";
+
+const SignInButton = () => {
+  const { pending } = useFormStatus();
+  return (
+    <Button disabled={pending} className="w-full" variant="default">
+      {pending ? "Signing In..." : "Sign In"}
+    </Button>
+  );
+};
+
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+
   return (
-    <form>
+    <form action={action}>
+      <input type='hidden' name="callbackUrl" value={callbackUrl} />
       <div className="space-y-6">
         <div>
-          <label htmlFor="email">Email</label>
+          <Label htmlFor="email">Email</Label>
           <Input
             id="email"
             name="email"
@@ -21,23 +45,29 @@ const CredentialsSignInForm = () => {
           />
         </div>
         <div>
-          <label htmlFor="email">Password</label>
+          <Label htmlFor="password">Password</Label>
           <Input
             id="password"
             name="password"
             type="password"
             required
-            autoComplete="password"
+            autoComplete="current-password"
             defaultValue={signInDefaultValues.password}
           />
         </div>
         <div>
-          <Button className="w-full" variant="default">
-            Sign-in
-          </Button>
+          <SignInButton />
         </div>
+
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+
         <div className="text-sm text-center text-muted-foreground">
-          Don&apos;t have an account? { ' ' }<Link href="/sign-up"> Sign Up</Link>
+          Don&apos;t have an account?{" "}
+          <Link href="/sign-up" target="_self" className="link">
+            Sign Up
+          </Link>
         </div>
       </div>
     </form>
