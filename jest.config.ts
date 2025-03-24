076d7b119b88484d3bd2b737184178f1837e031c
yadu <kyadushree47@gmail.com>
2025-03-24 14:10:30 +0530

    added jest

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+export default {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+  };
+  
