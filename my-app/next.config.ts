03847b7f5664292b65c853bc891ccc3c4a492a74
yadu <kyadushree47@gmail.com>
2025-03-24 15:07:56 +0530

    orders, admin , webhooks added

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from .env file
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET, // Ensure it's accessible
+  },
 };
 
 export default nextConfig;
